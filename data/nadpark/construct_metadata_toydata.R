a97907e83c30d4e45ac4509bbaed4d91c710680c
pglpm <pgl@portamana.org>
2024-08-03 16:54:38 +0200

    add scripts for toy inference

---
+++ data/nadpark/construct_metadata_toydata.R
@@ -0,0 +1,60 @@
+### We now construct the metadata file for the toydata.
+###
+### First we load the 'modelfreeinference' package.
+### Check the README in
+### https://github.com/pglpm/bayes_nonparametric_inference
+### for installation instructions.
+
+library('modelfreeinference')
+
+### We use the utility function 'buildmetadata()' to save a first version
+### of a metadata file, to work on:
+
+buildmetadata('toydata.csv', file = 'temp_metatoydata.csv',
+    excludevrt = 'Subject.Id')
+
+seed <- 16
+
+outputdir <- inferpopulation(
+    data = 'toydata.csv',
+    metadata = 'metatoydata.csv',
+    outputdir = 'toy_analysis_2',
+    output = 'directory',
+    appendtimestamp = F,
+    appendinfo = TRUE,
+    nsamples = 3600,
+    nchains = 60,
+    parallel = 4,
+    ## startupMCiterations = 1024,
+    ## maxhours = 2*1/60,
+    ## relerror = 0.02,
+    ncheckpoints = NULL,
+    cleanup = FALSE,
+    ## miniter = 1200,
+    prior = FALSE,
+    showKtraces = TRUE,
+    showAlphatraces = TRUE,
+    seed = seed,
+    hyperparams = list(
+        nclusters = 64,
+        minalpha = -4,
+        maxalpha = 4,
+        byalpha = 1,
+        Rshapelo = 0.5,
+        Rshapehi = 0.5,
+        Rvarm1 = 3^2,
+        Cshapelo = 0.5,
+        Cshapehi = 0.5,
+        Cvarm1 = 3^2,
+        Dshapelo = 0.5,
+        Dshapehi = 0.5,
+        Dvarm1 = 3^2,
+        Lshapelo = 0.5,
+        Lshapehi = 0.5,
+        Lvarm1 = 3^2,
+        Bshapelo = 1,
+        Bshapehi = 1,
+        Dthreshold = 1
+    )
+)
+
