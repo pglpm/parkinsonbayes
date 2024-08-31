347b8f19f49e15676a687714ca73bd92e84faf9d
pglpm <pgl@portamana.org>
2024-08-31 18:34:57 +0200

    update metadata format

--- data/nadpark/construct_metadata_toydata.R
+++ data/nadpark/construct_metadata_toydata.R
@@ -10,5 +10,5 @@ library('inferno')
 ### We use the utility function 'buildmetadata()' to save a first version
 ### of a metadata file, to work on:
 
-buildmetadata('toydata.csv', file = 'temp_metatoydata.csv',
+metadatatemplate('toydata.csv', file = 'temp_metatoydata.csv',
     excludevrt = 'Subject.Id')
