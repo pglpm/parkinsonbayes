7956436aa3bc2c7a7707df76e3d08362d008ed58
585090@stud.hvl.no <585090@stud.hvl.no>
2024-10-16 09:44:15 +0200

    First test for NAD-ATP graphs (not working)

--- data/nadpark/NAD_ATP_calculations.R
+++ data/nadpark/NAD_ATP_calculations.R
@@ -1,17 +1,24 @@
 library('inferno')
 
 parallel = 3
-
 learnt = 'output_learn_NAD-ratio'
 
-Y <- data.frame(TreatmentGroup = 'NR', Sex = 'male', Age = 40:80)
-X <- data.frame(visit_ratio = 0:2)
+X <- data.frame(TreatmentGroup = 'NR', Sex = 'male')
+Y <- data.frame(visit_ratio = 0:2)
 
 probabilities <- Pr(Y = Y, X = X, learnt = learnt, parallel = parallel)
 
-flexiplot(
-    x = X$visit_ratio,
+
+flexiplot(x = as.matrix(Y$visit_ratio),
     y = probabilities$values,
-    xlab = 'NAD visit ratio',
-    ylab = 'probability'
+    xlab = 'Visit ratio',
+    ylab = 'probability',
+    col = 3
+)
+
+plotquantiles(x = as.matrix(Y$visit_ratio),
+    y = probabilities$quantiles[,1,],
+    xlab = 'Visit ratio',
+    ylab = 'probability',
+    col = 2
 )
